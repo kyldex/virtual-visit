3bcc8ec65c45b28a363d5c766108113009b1838a
kyldex <raphael.giagnorio@gmail.com>
2021-06-11 00:06:30 +0200

    create new sphere when place changes

--- js/app.js
+++ js/app.js
@@ -1,14 +1,15 @@
 const container = document.body;
 
 /**
- * Wrapper to create a scene associated with a panoramic picture
+ * Wrapper for the scene with methods to add a sphere and direction buttons
  * @param image - the panoramic picture
  */
 class CustomSceneObject {
   constructor(image) {
     this.image = image;
-    this.scene = null;
     this.points = [];
+    this.scene = null;
+    this.sphere = null;
     this.sprites = [];
   }
 
@@ -16,18 +17,23 @@ class CustomSceneObject {
     this.points.push(point);
   }
 
-  addTooltip(point) {
+  addDirectionButton(point) {
     const spriteMap = new THREE.TextureLoader().load('../img/arrow-up.svg');
     const spriteMaterial = new THREE.SpriteMaterial({ map: spriteMap });
     const sprite = new THREE.Sprite(spriteMaterial);
     sprite.name = point.name;
     sprite.position.copy(point.position.clone().normalize().multiplyScalar(15));
     sprite.scale.multiplyScalar(0.85);
+    sprite.onClick = () => {
+      this.destroy();
+      point.nextCustomSceneObject.createSphere(this.scene);
+      point.nextCustomSceneObject.appear();
+    };
     this.scene.add(sprite);
     this.sprites.push(sprite);
   }
 
-  createScene(scene) {
+  createSphere(scene) {
     this.scene = scene;
     const geometry = new THREE.SphereGeometry(50, 32, 32);
     const texture = new THREE.TextureLoader().load(this.image);
@@ -35,31 +41,73 @@ class CustomSceneObject {
     texture.repeat.x = -1;
     const material = new THREE.MeshBasicMaterial({
       map: texture,
-      side: THREE.DoubleSide,
+      side: THREE.DoubleSide
     });
     material.transparent = true;
     this.sphere = new THREE.Mesh(geometry, material);
     this.scene.add(this.sphere);
-    this.points.forEach(this.addTooltip.bind(this));
+    this.points.forEach(this.addDirectionButton.bind(this));
+  }
+
+  appear() {
+    this.sphere.material.opacity = 0;
+    TweenLite.to(this.sphere.material, 1, { opacity: 1 });
+
+    this.sprites.forEach((sprite) => {
+      sprite.scale.set(0, 0, 0);
+      TweenLite.to(sprite.scale, 1, {
+        x: 1,
+        y: 1,
+        z: 1
+      });
+    });
+  }
+
+  destroy() {
+    TweenLite.to(this.sphere.material, 1, {
+      opacity: 0,
+      onComplete: () => {
+        this.scene.remove(this.sphere);
+      }
+    });
+
+    this.sprites.forEach((sprite) => {
+      TweenLite.to(sprite.scale, 1, {
+        x: 0,
+        y: 0,
+        z: 0,
+        onComplete: () => {
+          this.scene.remove(sprite);
+        }
+      });
+    });
   }
 }
 
-// Scenes
-const s1 = new CustomSceneObject('../img/outside.jpeg');
-const s2 = new CustomSceneObject('../img/inside.jpeg');
+// Scene
 const scene = new THREE.Scene();
+const CustomSceneObject1 = new CustomSceneObject('../img/outside.jpeg');
+const CustomSceneObject2 = new CustomSceneObject('../img/inside.jpeg');
 
-s1.addPoint({
-  position: new THREE.Vector3(47.64980841395977, -7.69998581077531, 12.086053677672625),
+CustomSceneObject1.addPoint({
+  position: new THREE.Vector3(
+    47.64980841395977,
+    -7.69998581077531,
+    12.086053677672625
+  ),
   name: 'Entrance',
-  scene: s2
+  nextCustomSceneObject: CustomSceneObject2
 });
-s2.addPoint({
-  position: new THREE.Vector3(47.64980841395977, -7.69998581077531, 12.086053677672625),
+CustomSceneObject2.addPoint({
+  position: new THREE.Vector3(
+    47.64980841395977,
+    -20,
+    120
+  ),
   name: 'Exit',
-  scene: s1
+  nextCustomSceneObject: CustomSceneObject1
 });
-s1.createScene(scene);
+CustomSceneObject1.createSphere(scene);
 
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -84,12 +132,14 @@ function animate() {
 }
 animate();
 
+// Makes the website responsive
 function onResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
 }
 
+// Triggers the sprite's onClick callback
 function onClick(e) {
   const mouse = new THREE.Vector2(
     (e.clientX / window.innerWidth) * 2 - 1,
@@ -100,17 +150,9 @@ function onClick(e) {
   const intersects = raycaster.intersectObjects(scene.children);
   intersects.forEach((intersect) => {
     if (intersect.object.type === 'Sprite') {
-      TweenLite.to(sphere.material, 1, {
-        opacity: 0
-      });
+      intersect.object.onClick();
     }
   });
-
-  // const intersects = raycaster.intersectObject(sphere);
-  // if (intersects.length > 0) {
-  //   console.log(intersects[0].point);
-  //   addTooltip(intersects[0].point);
-  // }
 }
 
 window.addEventListener('resize', onResize);
